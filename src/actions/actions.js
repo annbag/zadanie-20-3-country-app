e7345ae9e069615855ef342e52733849890d1f68
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-04 15:55:48 +0200

    add actions

---
+++ src/actions/actions.js
@@ -0,0 +1,7 @@
+export const GET_COUNTRIES = 'GET_COUNTRIES'; //listowanie wszytkich dostępnych państw
+export const GET_COUNTRY = 'GET_COUNTRY'; // wyświetlanie informacji o jednym państwie;
+export const DELETE_COUNTRY = 'DELETE_COUNTRY'; //usuwanie państwa
+export const SEARCH_COUNTRIES = 'SEARCH_COUNTRIES'; //szukanie państwa po nazwie
+export const SET_CONTINENT = 'SET_CONTINENT'; // wyświetlanie państw konktretnego kontynentu
+
+
